40f722d209cbc5ea1bd73f51396899efd331adda
saranoe7-coder <saranoe7@gmail.com>
2026-06-17 20:19:31 +0200

    sync: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import base44Plugin from "@base44/vite-plugin";
+import { base44Plugin } from '@base44/vite-plugin'
 
 export default defineConfig({
- plugins: [
-  react()
-]
+  plugins: [
+    base44Plugin(),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

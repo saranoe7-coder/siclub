4c9c82ce68d0a631429e517ef44227f46b959671
saranoe7-coder <saranoe7@gmail.com>
2026-06-17 21:00:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PageNotFound from './lib/PageNotFound';
+
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Home from './pages/Home';
@@ -35,7 +35,6 @@ const AuthenticatedApp = () => {
       <Route path="/" element={<Home />} />
       <Route path="/rezervacia" element={<Rezervacia />} />
       <Route path="/github-sync" element={<GithubSync />} />
-      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };

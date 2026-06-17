74b095b89397dc1d0fadae4f26b7c12d4d368196
saranoe7-coder <saranoe7@gmail.com>
2026-06-17 20:08:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { base44Plugin } from '@base44/vite-plugin'
+import base44Plugin from "@base44/vite-plugin";
 
 export default defineConfig({
   plugins: [

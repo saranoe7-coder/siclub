db3802aa376fbc1a0e6ca7285147d6d2855b06b8
saranoe7-coder <saranoe7@gmail.com>
2026-06-17 19:44:55 +0200

    sync: update src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,29 @@
 import Navbar from '../components/siclub/Navbar';
-export default function Home() { return <div><Navbar /></div>; }
+import Hero from '../components/siclub/Hero';
+import TargetAudience from '../components/siclub/TargetAudience';
+import HowItWorks from '../components/siclub/HowItWorks';
+import Services from '../components/siclub/Services';
+import ClientResults from '../components/siclub/ClientResults';
+import Team from '../components/siclub/Team';
+import Partners from '../components/siclub/Partners';
+import FinalCTA from '../components/siclub/FinalCTA';
+import Footer from '../components/siclub/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background font-inter">
+      <Navbar />
+      <main>
+        <Hero />
+        <TargetAudience />
+        <HowItWorks />
+        <Services />
+        <ClientResults />
+        <Team />
+        <FinalCTA />
+        <Partners />
+      </main>
+      <Footer />
+    </div>
+  );
+}
